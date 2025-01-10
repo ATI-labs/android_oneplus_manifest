5e0793f25424b402331ce0b969991419e2d08c64
Arman Altafi <84268732+Arman-ATI@users.noreply.github.com>
2025-01-10 21:08:01 +0330

    Update fix.sh

--- fix.sh
+++ fix.sh
@@ -1,3 +1,3 @@
 echo "Fixing boot problem on oplus sm8450 devices"
-rm -r hardware/qcom-caf/sm8450/audio/agm && rm -r hardware/qcom-caf/sm8450/display && git clone https://github.com/Arman-ATI/hardware_qcom-caf_sm8450_audio_agm.git hardware/qcom-caf/sm8450/audio/agm && git clone https://github.com/Arman-ATI/hardware_qcom-caf_sm8450_display.git hardware/qcom-caf/sm8450/display
+rm -rf hardware/qcom-caf/sm8450/audio/agm && rm -rf hardware/qcom-caf/sm8450/display && rm -rf hardware/qcom-caf/sm8450/pal && git clone https://github.com/Arman-ATI/hardware_qcom-caf_sm8450_audio_agm.git hardware/qcom-caf/sm8450/audio/agm && git clone https://github.com/Arman-ATI/hardware_qcom-caf_sm8450_display.git hardware/qcom-caf/sm8450/display && git clone https://github.com/Arman-ATI/android_vendor_qcom_opensource_arpal-lx.git hardware/qcom-caf/sm8450/pal
 echo "Done!!!"
